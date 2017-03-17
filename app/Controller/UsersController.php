24b52c05ddb459967dd1bf75bcbd4b6646aa63f5
Su <su.nq@barista.co.jp>
2017-03-17 16:32:53 +0700

    ok

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -20,4 +20,29 @@ class UsersController extends AppController
     public function admin_logout(){
         $this->redirect($this->Auth->logout());
     }
+    public function admin_list(){
+        //Lay du lieu trong table users
+        $array_user = $this->User->find('all', array(
+            'conditions'=>array('id > 0'),
+            'recursive'   =>-1
+        ));
+        //đưa dữ liệu lấy được qua view bằng biến users
+        $this->set('users', $array_user);
+    }
+    public function admin_delete($id = null){
+        if($this->request->is('get')){
+            $data = $this->User->find('first', array(
+                'fields' => array('id','name'),
+                'conditions'=>array('id'=>$id),
+                'recursive'=>-1
+            ));
+            if(!empty($data)){
+                $this->Session->setFlash('Success');
+                //$this->User->delete($id);
+            }else{
+                $this->Session->setFlash('Error');
+            }
+            $this->redirect(array('action'=>'list'));
+        }
+    }
 }
